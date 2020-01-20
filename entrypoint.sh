b83a75309940cba732613f21a4e8df11a3959925
birme <jonas.birme@eyevinn.se>
2020-01-20 21:53:46 +0100

    Kube config parameters were never used

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 set -e
-REGION=eu-north-1
-CLUSTER=prod
+: ${REGION:=eu-north-1} ${CLUSTER:=prod}
 
 export PATH=$PATH:/usr/local/bin/kubectl:/usr/local/bin/aws-iam-authenticator:/usr/local/bin/aws
 
